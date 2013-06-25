1b6b116adb19912393ff4d11457418ccec46b07c
mathfuru <mathfuru@gmail.com>
2013-06-25 12:38:34 +0900

    パーサ修正

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -14,20 +14,16 @@ import Parser
 
 getHomeR :: Handler RepHtml
 getHomeR = do
-    let links = [("pages", "users"), ("users", "confs")]
     lines <- liftIO $ (parse . map unpack) <$> (split (== '\n') <$> pack <$> readFile "table_def.dot")
-    let table_names = getTableNames lines
-    let links' = catMaybes $ (convertTableNameToIndex $ map pack table_names) <$> links
-    let links'' = map (\(src, dst) -> object ["source" .= src, "target" .= dst]) links'
-    let json = object ["nodes" .= toJSON table_names, "links" .= toJSON links'' ]
+    let defs = getTableDef lines
+    let links = getLinkIndexs lines
+    let json = object ["nodes" .= toJSON defs, "links" .= toJSON (map convertToSourceAndTarget links)]
+    liftIO $ print json
     defaultLayout $ do
          toWidget [julius| var graph = #{json} |]
 
-convertTableNameToIndex :: [Text] -> (Text, Text) -> Maybe (Int, Int)
-convertTableNameToIndex names (name1, name2) = do
-  l <- elemIndex name1 names
-  r <- elemIndex name2 names
-  return (l, r)
+convertToSourceAndTarget :: (Int, Int) -> Value
+convertToSourceAndTarget (i, j) = object ["source" .= toJSON i, "target" .= toJSON j]
 
 postHomeR :: Handler RepHtml
 postHomeR = do
